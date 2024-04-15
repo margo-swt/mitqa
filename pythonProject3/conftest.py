59af7e8ebe6a23a849867009b2d2543bb07a64d6
mariqa <mariami.goginava.li@gmail.com>
2024-04-15 23:06:07 +0400

    postman

--- pythonProject3/conftest.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-
-class DriverSetUp:
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://codenboxautomationlab.com/registration-form/")
-    # driver.implicitly_wait(10)
